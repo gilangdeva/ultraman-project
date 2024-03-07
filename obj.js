825cfa1919bc34be6b6deaff877a7af8a0f48453
Gilang Deva <gilangdevaa@gmail.com>
2024-03-07 09:13:05 +0700

    First Commit

---
+++ obj.js
@@ -0,0 +1,15 @@
+const ultraman = {
+    name: "tiga",
+    height: 100,
+    isDead: false,
+    address: {
+        planet: {
+            name: "Namex",
+            moon: [83930, 83030, 674784]
+        },
+        galaxy: "Andromeda 12",
+    },
+    weapons: ["Sword", "Shield", "Lasso"]
+}
+
+console.log(ultraman.address.planet);
